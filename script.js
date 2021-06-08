1db0f8681875aa3e26d148525c893aa54093998c
MedMaisTi <81438352+MedMaisTi@users.noreply.github.com>
2021-06-08 11:34:34 -0300

    dsfds

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-window.onload = function(){
-    var slides = document.getElementsByClassName('carousel-item'),
-        addActive = function(slide) {slide.classList.add('active')},
-        removeActive = function(slide) {slide.classList.remove('active')};
-    addActive(slides[0]);
-    
-    setInterval(function (){
-      for (var i = 0; i < slides.length; i++){
-        if (i + 1 == slides.length) {
-          addActive(slides[0]);
-          slides[0].style.zIndex = 100;
-          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
-          break;
-        }
-        if (slides[i].classList.contains('active')) { 
-          slides[i].removeAttribute('style');
-          setTimeout(removeActive, 350, slides[i]); //Doesn't be worked in IE-9
-          addActive(slides[i + 1]);
-          break;
-        }
-      } 
-    }, 300000);
-  }
-    
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+  .fadeOut(1000)
+  .next()
+  .fadeIn(1000)
+  .end()
+  .appendTo('#slideshow');
+}, 3000);
